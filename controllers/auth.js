1990adaed1cce787f769553a37d53cfea1b15d98
haddadoz89 <haddadoz1989@gmail.com>
2025-07-10 13:28:28 +0300

    uppercase problem while deploying

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require("../models/user");
+const User = require("../models/User");
 const HealthCenter = require("../models/healthCenter");
 const bcrypt = require("bcrypt");
 
